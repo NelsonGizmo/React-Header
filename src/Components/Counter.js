3a321c80115d5d9f7742bbc02d5387ba3cd935a8
NelsonGizmo <115164302+NelsonGizmo@users.noreply.github.com>
2022-11-24 12:38:31 +0100

    some changes has been made to the counter button tagged change name

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
-    
+    const [state, setState] = useState("Allen West");
+
+    function changeName(){
+        setState("GoodGod Okorie");
+    }
+
   return (
     <div>
         <h1>Counter App</h1>
-        <p>Allen West</p>
-        <button>Change Name</button>
+        <p>{state}</p>
+        <button onClick={changeName}>Change Name</button>
+        <button>Increment</button>
+        <button>Decreement</button>
     </div>
   )
 }
